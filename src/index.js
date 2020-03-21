0ff8414a2a4fa55342b2daa069a9041e42534f52
lukasz-zygmanski <59967847+lukasz-zygmanski@users.noreply.github.com>
2020-03-21 20:46:09 +0100

    chore: brackets solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = (str, bracketsConfig) => {
+  const brackets = bracketsConfig.map(elem => elem.join(''))
+
+  const removeBrackets = (str) => {
+    const replacedString = brackets.reduce((acc, item) => acc.replace(item, ''), str);
+    
+    return replacedString === str ? replacedString : removeBrackets(replacedString);
+  }
+  return !!!removeBrackets(str).length;
 }
